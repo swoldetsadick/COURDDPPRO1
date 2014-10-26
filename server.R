14066986459d3a2cdfef3a3e98230fc8e0da2726
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-10-26 06:10:20 +0100

    messages

--- server.R
+++ server.R
@@ -62,7 +62,7 @@ shinyServer(
                         m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
                         print(m)} else if (input$distri == 4) {
                                 
-                        a <- matrix(rexp((input$id1 * input$nval), input$five), input$id1, input$nval)
+                        a <- matrix(rexp((input$id1 * input$nval), input$six), input$id1, input$nval)
                         b <- apply(a, 1, mean)
                         c <- round(mean(apply(a, 1, mean)), digits = 2)
                         d <- 1/input$six
@@ -72,7 +72,25 @@ shinyServer(
                         subtitle <- 'Samples drawn from Exponential Distribution'
                                 
                         m <- ggplot(data.frame(b), aes(b))
-                        m <- m + geom_histogram(aes(y =..density..), binwidth = (round(max(b)-min(b))/40), color = "black", fill = "white")
+                        m <- m + geom_histogram(aes(y =..density..), color = "black", fill = "white")
+                        m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
+                        m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
+                        m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
+                        m <- m + geom_text(x = Inf, y = Inf, label = paste(paste(paste("Real Mean mu =", d, sep=" "),"\nExpected Value of Averages =", sep=" "), c, sep = " "), hjust = 1, vjust = 1)
+                        m <- m + geom_vline(aes(xintercept = round(mean(data.frame(b)$b), digits = 2)), color = "blue", linetype = "dashed", size = 1)        
+                        print(m)} else {
+                                
+                        a <- matrix(runif((input$id1 * input$nval)), input$id1, input$nval)
+                        b <- apply(a, 1, mean)
+                        c <- round(mean(apply(a, 1, mean)), digits = 2)
+                        d <- 1/2
+                        e <- sqrt(1/12)
+                                
+                        title <- 'Distribution of Sample Means (Theoretical vs Simulated)'
+                        subtitle <- 'Samples drawn from Uniform Distribution'
+                                
+                        m <- ggplot(data.frame(b), aes(b))
+                        m <- m + geom_histogram(aes(y =..density..), color = "black", fill = "white")
                         m <- m + xlab("Sample Averages") + ylab("Density") + ggtitle(bquote(atop(.(title), atop(italic(.(subtitle)), "")))) 
                         m <- m + geom_density(col="red", alpha=.2, fill="#FF6666") + theme(plot.title = element_text(size=19,lineheight=.8,vjust=-1))
                         m <- m + stat_function(fun = dnorm, args = list(mean = d, sd = (e/sqrt(input$nval))), color = "blue")
--- ui.R
+++ ui.R
@@ -2,6 +2,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
         headerPanel("Illustrating Central Limit Theorem (CLT)"),
         sidebarPanel(
+                img(src="plevy.png", height = 400, width = 400),
+
                 radioButtons("distri", label = h4("Distribution Type"),
                         choices = list("Normal Distribution" = 1, "Uniform Distribution" = 2, 
                                         "Student Distribution" = 3, "Exponential Distribution" = 4, 
@@ -15,23 +17,27 @@ shinyUI(pageWithSidebar(
                                  numericInput("four", "Scale of Distribution:", 1, min = 1)),
                 
                 conditionalPanel(condition = "input.distri == 3", label = h4("Distribution Parameters Choice"),
-                                 numericInput("five", "Degree of Freedom of Distribution:", 2, min = 2)),
+                                 numericInput("five", "Degree of Freedom of Distribution:", 3, min = 2)),
                 
                 conditionalPanel(condition = "input.distri == 4", label = h4("Distribution Parameters Choice"),
-                                 numericInput("six", "Rate of Distribution:", 0)),
+                                 numericInput("six", "Rate of Distribution:", 1, min =1)),
                 
                 sliderInput('nval', 'Number of Observations (n)', 2, min = 1, max = 100, step = 1,),
                 
-                numericInput('id1', 'Number of Simulations', 1000, min = 0, max = 100000, step = 100)  
+                numericInput('id1', 'Number of Simulations', 1000, min = 0, max = 100000, step = 100)
+                
         ),
         
         mainPanel(
-                h4('Illustrating outputs'),
+                h6('In probability theory, the central limit theorem (CLT) states that, the arithmetic mean of a sufficiently large number of iterates of independent random variables, each with a well-defined expected value and well-defined variance, will be approximately normally distributed, regardless of the underlying distribution.'),
+                h6('To illustrate this theorem, choose any distribution from the list in the side panel. Set the number of sampling iteration in the box labeled "Number of Simulation" then set parameters, whose number depend on the distribution selected.'),
+                h6('Use the slider in the side bar to raise or lower the number to observations in a sample, and observe in the graph how this affects the distribution of the arithmetic averages of the sample. The density for this distribution is drawn in full red line, with a histogram set for arithmetic averages.'),
+                h6('Observe how as you raise the number of observations in samples, or number of iteration the density of said distribution closely tends to its corresponding central limit theorem normal distribution.'),
+                h6('Expected value of these averages approaches that of the real value of the mean of the distribution from which these observations are sampled of.'),
                 h6('The chosen distribution is:'),
                 verbatimTextOutput("odistri"),
                 h6('Number of simulations chosen is:'), 
                 verbatimTextOutput("oid1"),
                 plotOutput("xbar")
-                
         )
 ))
